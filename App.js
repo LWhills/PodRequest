89166abfcb6250a0cb12e15e064708710b933bdf
LoganW <logan.willaumez@icloud.com>
2021-07-28 17:01:13 +0200

    Add the JS element

---
+++ App.js
@@ -0,0 +1,19 @@
+
+function emailVerification(){
+let email = document.querySelector('.input-email').value;
+let submit = document.querySelector('.input-submit');
+let text = document.querySelector('.email-validation')
+let template = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(template)){
+      text.innerHTML = "Your E-mail is valid"
+      text.style.color ="green";
+  }
+  else if (email == ""){
+      text.innerHTML = "It's empty, fill up your E-mail adress please."
+  }
+  else{
+      console.log("NotOk")
+      text.innerHTML = "Oops! please check your E-mail"
+  }
+}
+  
